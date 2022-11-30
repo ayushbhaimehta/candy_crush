d65d6883e299c226d625da756fe24d9fb5f5c5f7
ayushbhaimehta <ayushbhaimehta20002@gmail.com>
2022-11-30 20:05:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,56 @@ const color = [
 const App = () => {
   const [currColor, setCurrColor] = useState([]);
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + 8, i + 16, i + 24]
+      const decidedColor = currColor[i]
+
+      if (columnOfFour.every(ind => currColor[ind] === decidedColor)) {
+        columnOfFour.forEach(ind => currColor[ind] = ' ');
+      }
+    }
+  }
+
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++) {
+
+      if (i % 8 != 5 || i % 8 != 6 || i % 8 != 7) {
+        const RowOfFour = [i, i + 1, i + 2, i + 3]
+        const decidedColor = currColor[i]
+
+        if (RowOfFour.every(ind => currColor[ind] === decidedColor)) {
+          RowOfFour.forEach(ind => currColor[ind] = ' ');
+        }
+      }
+    }
+  }
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + 8, i + 16]
+      const decidedColor = currColor[i]
+
+      if (columnOfThree.every(ind => currColor[ind] === decidedColor)) {
+        columnOfThree.forEach(ind => currColor[ind] = ' ');
+      }
+    }
+  }
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+
+      if (i % 8 != 6 || i % 8 != 7) {
+        const RowOfThree = [i, i + 1, i + 2]
+        const decidedColor = currColor[i]
+
+        if (RowOfThree.every(ind => currColor[ind] === decidedColor)) {
+          RowOfThree.forEach(ind => currColor[ind] = ' ');
+        }
+      }
+    }
+  }
+
   const createBoard = () => {
     const colorArray = [];
     for (let i = 0; i < 64; i++) {
@@ -27,6 +77,19 @@ const App = () => {
   useEffect(() => {
     createBoard();
   }, []);
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForRowOfFour();
+      checkForColumnOfThree();
+      checkForRowOfThree();
+      setCurrColor([...currColor])
+    }, 100)
+    return () => clearInterval(timer)
+  }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, currColor]);
+
   console.log(currColor);
 
   return (
